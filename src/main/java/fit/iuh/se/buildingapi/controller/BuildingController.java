90d8e13220d9a45c2f1c50e7af539f76d1c17671
phatdang <phatdang19052004@gmail.com>
2025-07-22 19:20:36 +0700

    Xây dựng lớp BuildingController

---
+++ src/main/java/fit/iuh/se/buildingapi/controller/BuildingController.java
@@ -0,0 +1,26 @@
+package fit.iuh.se.buildingapi.controller;
+
+import fit.iuh.se.buildingapi.dto.response.BuildingReponse;
+import fit.iuh.se.buildingapi.service.BuildingService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("buildings")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+
+public class BuildingController {
+    BuildingService buildingService;
+
+    @GetMapping
+    List<BuildingReponse> getAllBuidings(){
+        return buildingService.getAllBuidings();
+    }
+}
