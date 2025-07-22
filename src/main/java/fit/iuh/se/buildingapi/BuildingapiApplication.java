ad962404b415462fc19abef23a626c16a084cfc6
phatdang <phatdang19052004@gmail.com>
2025-07-22 18:01:29 +0700

    first commit

---
+++ src/main/java/fit/iuh/se/buildingapi/BuildingapiApplication.java
@@ -0,0 +1,13 @@
+package fit.iuh.se.buildingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingapiApplication.class, args);
+    }
+
+}
