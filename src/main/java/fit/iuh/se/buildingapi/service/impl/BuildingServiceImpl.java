94dd11fca366bd8404e54186d8f1fa2035e819d2
phatdang <phatdang19052004@gmail.com>
2025-07-22 19:21:19 +0700

    Xây dựng lớp BuildingServiceImpl

---
+++ src/main/java/fit/iuh/se/buildingapi/service/impl/BuildingServiceImpl.java
@@ -0,0 +1,29 @@
+package fit.iuh.se.buildingapi.service.impl;
+
+import fit.iuh.se.buildingapi.dto.response.BuildingReponse;
+import fit.iuh.se.buildingapi.mapper.BuildingMapper;
+import fit.iuh.se.buildingapi.repository.BuildingRepository;
+import fit.iuh.se.buildingapi.service.BuildingService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+
+public class BuildingServiceImpl implements BuildingService {
+    BuildingRepository buildingRepository;
+    BuildingMapper buildingMapper;
+    @Override
+    public List<BuildingReponse> getAllBuidings() {
+        return buildingRepository.findAll()
+                .stream()
+                .map(buildingMapper::toBuildingReponse)
+                .collect(Collectors.toList());
+    }
+}
