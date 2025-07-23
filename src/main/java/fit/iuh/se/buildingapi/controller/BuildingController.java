81a05db3a0e9001158469a0125b8a94464e1f427
phatdang <phatdang19052004@gmail.com>
2025-07-23 10:27:08 +0700

    Cập nhật lớp BuildingController

--- src/main/java/fit/iuh/se/buildingapi/controller/BuildingController.java
+++ src/main/java/fit/iuh/se/buildingapi/controller/BuildingController.java
@@ -1,13 +1,13 @@
 package fit.iuh.se.buildingapi.controller;
 
+import fit.iuh.se.buildingapi.dto.request.BuildingCreationRequest;
+import fit.iuh.se.buildingapi.dto.request.BuildingUpdationRequest;
 import fit.iuh.se.buildingapi.dto.response.BuildingResponse;
 import fit.iuh.se.buildingapi.service.BuildingService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +20,22 @@ public class BuildingController {
     BuildingService buildingService;
 
     @GetMapping
-    List<BuildingResponse> getAllBuidings(){
+    List<BuildingResponse> getAllBuidings() {
         return buildingService.getAllBuidings();
+    }
+
+    @PostMapping
+    BuildingResponse addBuilding(@RequestBody BuildingCreationRequest request) {
+        return buildingService.addBuilding(request);
+    }
+
+    @DeleteMapping("/{buildingId}")
+    void deleteBuilding(@PathVariable Integer buildingId) {
+        buildingService.deleteBuilding(buildingId);
+    }
+
+    @PutMapping("/{buildingId}")
+    BuildingResponse updateBuilding(@PathVariable Integer buildingId, @RequestBody BuildingUpdationRequest request) {
+        return buildingService.updateBuilding(buildingId, request);
     }
 }
