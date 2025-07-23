d75a44969424ed89972e4e7fd386eb115c64de76
phatdang <phatdang19052004@gmail.com>
2025-07-23 10:27:24 +0700

    Cập nhật lớp BuildingService

--- src/main/java/fit/iuh/se/buildingapi/service/BuildingService.java
+++ src/main/java/fit/iuh/se/buildingapi/service/BuildingService.java
@@ -1,9 +1,17 @@
 package fit.iuh.se.buildingapi.service;
 
+import fit.iuh.se.buildingapi.dto.request.BuildingCreationRequest;
+import fit.iuh.se.buildingapi.dto.request.BuildingUpdationRequest;
 import fit.iuh.se.buildingapi.dto.response.BuildingResponse;
+import fit.iuh.se.buildingapi.entity.Building;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface BuildingService {
     List<BuildingResponse> getAllBuidings();
+    BuildingResponse addBuilding(@RequestBody BuildingCreationRequest request);
+    void deleteBuilding(@PathVariable Integer id);
+    BuildingResponse updateBuilding(@PathVariable Integer buildingId, @RequestBody BuildingUpdationRequest request);
 }
