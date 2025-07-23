012727dfaf7e66d251c47ba272186aa6ca112e0d
phatdang <phatdang19052004@gmail.com>
2025-07-23 10:27:16 +0700

    Cập nhật lớp BuildingMapper

--- src/main/java/fit/iuh/se/buildingapi/mapper/BuildingMapper.java
+++ src/main/java/fit/iuh/se/buildingapi/mapper/BuildingMapper.java
@@ -1,8 +1,12 @@
 package fit.iuh.se.buildingapi.mapper;
 
+import fit.iuh.se.buildingapi.dto.request.BuildingCreationRequest;
+import fit.iuh.se.buildingapi.dto.request.BuildingUpdationRequest;
 import fit.iuh.se.buildingapi.dto.response.BuildingResponse;
 import fit.iuh.se.buildingapi.entity.Building;
+import fit.iuh.se.buildingapi.entity.District;
 import fit.iuh.se.buildingapi.entity.RentArea;
+import fit.iuh.se.buildingapi.service.DistrictRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -17,6 +21,7 @@ import java.util.stream.Collectors;
 
 public class BuildingMapper {
     ModelMapper mapper;
+    DistrictRepository districtRepository;
 
     public BuildingResponse toBuildingReponse(Building building) {
         BuildingResponse buildingResponse = mapper.map(building, BuildingResponse.class);
@@ -33,5 +38,46 @@ public class BuildingMapper {
         buildingResponse.setRentArea(rentArea);
 
         return buildingResponse;
+    }
+
+    public BuildingResponse toBuildingReponse(BuildingCreationRequest request) {
+        BuildingResponse buildingResponse = mapper.map(request, BuildingResponse.class);
+        buildingResponse.setRentArea("");
+        return buildingResponse;
+    }
+
+    public Building toBuilding(BuildingCreationRequest request){
+        Building building = mapper.map(request, Building.class);
+
+        String[] address = request.getAddress()
+                        .split(",");
+
+        building.setWard(address[0]);
+        building.setStreet(address[1]);
+
+        String districtName = address[2];
+        District district = districtRepository.findByName(districtName);
+
+        building.setDistrict(district);
+
+        return  building;
+    }
+
+    public Building toBuilding(Building building, BuildingUpdationRequest request){
+
+        building.setName(request.getName());
+
+        String[] address = request.getAddress()
+                .split(",");
+
+        building.setWard(address[0]);
+        building.setStreet(address[1]);
+
+        String districtName = address[2];
+        District district = districtRepository.findByName(districtName);
+
+        building.setDistrict(district);
+
+        return  building;
     }
 }
