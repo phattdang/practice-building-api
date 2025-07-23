28ae586829f6a440ef920a752ad94d44e0ec8536
phatdang <phatdang19052004@gmail.com>
2025-07-23 10:27:32 +0700

    Cập nhật lớp BuildingServiceImpl

--- src/main/java/fit/iuh/se/buildingapi/service/impl/BuildingServiceImpl.java
+++ src/main/java/fit/iuh/se/buildingapi/service/impl/BuildingServiceImpl.java
@@ -1,6 +1,9 @@
 package fit.iuh.se.buildingapi.service.impl;
 
+import fit.iuh.se.buildingapi.dto.request.BuildingCreationRequest;
+import fit.iuh.se.buildingapi.dto.request.BuildingUpdationRequest;
 import fit.iuh.se.buildingapi.dto.response.BuildingResponse;
+import fit.iuh.se.buildingapi.entity.Building;
 import fit.iuh.se.buildingapi.mapper.BuildingMapper;
 import fit.iuh.se.buildingapi.repository.BuildingRepository;
 import fit.iuh.se.buildingapi.service.BuildingService;
@@ -19,11 +22,31 @@ import java.util.stream.Collectors;
 public class BuildingServiceImpl implements BuildingService {
     BuildingRepository buildingRepository;
     BuildingMapper buildingMapper;
+
     @Override
     public List<BuildingResponse> getAllBuidings() {
         return buildingRepository.findAll()
                 .stream()
                 .map(buildingMapper::toBuildingReponse)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public BuildingResponse addBuilding(BuildingCreationRequest request) {
+        Building building = buildingMapper.toBuilding(request);
+        return buildingMapper.toBuildingReponse(buildingRepository.save(building));
+    }
+
+    @Override
+    public void deleteBuilding(Integer id) {
+        buildingRepository.deleteById(id);
+    }
+
+    @Override
+    public BuildingResponse updateBuilding(Integer buildingId, BuildingUpdationRequest request) {
+        Building building = buildingRepository.findById(buildingId)
+                .orElseThrow(() -> new RuntimeException("Building not found!"));
+
+        return buildingMapper.toBuildingReponse(buildingMapper.toBuilding(building, request));
     }
 }
