5ac1d72879f6f69278dcce350e461c77b6e491cc
phatdang <phatdang19052004@gmail.com>
2025-07-24 15:33:09 +0700

    feat(service): thêm chức năng getAllBuildingsV2
    Lấy danh sách theo tiêu chí tìm kiếm

--- src/main/java/fit/iuh/se/buildingapi/service/BuildingService.java
+++ src/main/java/fit/iuh/se/buildingapi/service/BuildingService.java
@@ -3,14 +3,16 @@ package fit.iuh.se.buildingapi.service;
 import fit.iuh.se.buildingapi.dto.request.BuildingCreationRequest;
 import fit.iuh.se.buildingapi.dto.request.BuildingUpdationRequest;
 import fit.iuh.se.buildingapi.dto.response.BuildingResponse;
-import fit.iuh.se.buildingapi.entity.Building;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BuildingService {
-    List<BuildingResponse> getAllBuidings();
+    List<BuildingResponse> getAllBuildings();
+    List<BuildingResponse> getAllBuildingsV2(@RequestParam Map<String, Object> params);
     BuildingResponse addBuilding(@RequestBody BuildingCreationRequest request);
     void deleteBuilding(@PathVariable Integer id);
     BuildingResponse updateBuilding(@PathVariable Integer buildingId, @RequestBody BuildingUpdationRequest request);
