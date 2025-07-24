0f5bedb529453de60ab798415e0c3bbabf42892c
phatdang <phatdang19052004@gmail.com>
2025-07-24 15:30:16 +0700

    feat(dto/request): thêm validation cho field name
    Kiểm tra kích thước tối thiểu

--- src/main/java/fit/iuh/se/buildingapi/dto/request/BuildingCreationRequest.java
+++ src/main/java/fit/iuh/se/buildingapi/dto/request/BuildingCreationRequest.java
@@ -1,5 +1,6 @@
 package fit.iuh.se.buildingapi.dto.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -11,6 +12,8 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 
 public class BuildingCreationRequest {
+    @NonNull
+    @Size(min = 8, message = "INVALID_NAME")
     String name;
     String address;
     Integer numberOfBasement;
