95c97cc42e07f28fbd1f230ca43be47922c02ccc
phatdang <phatdang19052004@gmail.com>
2025-07-24 15:32:32 +0700

    feat(repository): thêm chức năng existsByName
    Kiểm tra tồn tại của đối tượng qua name

--- src/main/java/fit/iuh/se/buildingapi/repository/BuildingRepository.java
+++ src/main/java/fit/iuh/se/buildingapi/repository/BuildingRepository.java
@@ -1,9 +1,11 @@
 package fit.iuh.se.buildingapi.repository;
 
 import fit.iuh.se.buildingapi.entity.Building;
+import fit.iuh.se.buildingapi.repository.custom.BuildingRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BuildingRepository extends JpaRepository<Building, Integer> {
+public interface BuildingRepository extends JpaRepository<Building, Integer>, BuildingRepositoryCustom {
+    boolean existsByName(String name);
 }
