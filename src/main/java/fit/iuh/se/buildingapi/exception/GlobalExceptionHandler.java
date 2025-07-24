c7f92baea401d9ccc19bfb8074a6917ea361b8a8
phatdang <phatdang19052004@gmail.com>
2025-07-24 15:34:45 +0700

    feat(exception): thêm các handler của các exception
    Thêm MethodArgumentNotValidException và AppException

--- src/main/java/fit/iuh/se/buildingapi/exception/GlobalExceptionHandler.java
+++ src/main/java/fit/iuh/se/buildingapi/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package fit.iuh.se.buildingapi.exception;
 import fit.iuh.se.buildingapi.dto.response.ApiResponse;
 import fit.iuh.se.buildingapi.entity.enums.HttpCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,6 +15,31 @@ public class GlobalExceptionHandler {
 
         apiResponse.setCode(HttpCode.BAD_REQUEST.getCODE());
         apiResponse.setMessage(exception.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<ApiResponse> handlingMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        ApiResponse apiResponse = new ApiResponse();
+
+        String errorKey = exception.getFieldError().getDefaultMessage();
+        HttpCode httpCode = HttpCode.valueOf(errorKey);
+
+        apiResponse.setCode(httpCode.getCODE());
+        apiResponse.setMessage(httpCode.getMESSAGE());
+
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse> handlingAppException(AppException exception){
+        ApiResponse apiResponse = new ApiResponse();
+
+        HttpCode httpCode = exception.getHttpCode();
+
+        apiResponse.setCode(httpCode.getCODE());
+        apiResponse.setMessage(httpCode.getMESSAGE());
 
         return ResponseEntity.badRequest().body(apiResponse);
     }
