75c53d0c6989988a80bc45eaef09c954fee1168e
phatdang <phatdang19052004@gmail.com>
2025-08-15 20:43:53 +0700

    feat(controller): thêm class UserController
    Thêm class UserController để xử lý các yêu cầu của User

---
+++ src/main/java/fit/iuh/se/buildingapi/controller/UserController.java
@@ -0,0 +1,53 @@
+package fit.iuh.se.buildingapi.controller;
+
+import fit.iuh.se.buildingapi.dto.request.AuthenticationRequest;
+import fit.iuh.se.buildingapi.dto.request.UserCreationRequest;
+import fit.iuh.se.buildingapi.dto.response.ApiResponse;
+import fit.iuh.se.buildingapi.dto.response.AuthenticationResponse;
+import fit.iuh.se.buildingapi.dto.response.UserCreationResponse;
+import fit.iuh.se.buildingapi.entity.enums.HttpCode;
+import fit.iuh.se.buildingapi.service.AuthenticationService;
+import fit.iuh.se.buildingapi.service.UserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class UserController {
+    UserService userService;
+    AuthenticationService authenticationService;
+
+    @PostMapping
+    ApiResponse<UserCreationResponse> addUser(@RequestBody UserCreationRequest request){
+        return ApiResponse.<UserCreationResponse>builder()
+                .code(HttpCode.OK.getCODE())
+                .message(HttpCode.OK.getMESSAGE())
+                .value(userService.addUser(request))
+                .build();
+    }
+
+    @PostMapping("/log-in")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        boolean result = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>
+                builder()
+                .code(HttpCode.OK.getCODE())
+                .message(HttpCode.OK.getMESSAGE())
+                .value(
+                        AuthenticationResponse
+                                .builder()
+                                .authenticated(result)
+                                .build()
+                )
+                .build();
+    }
+}
