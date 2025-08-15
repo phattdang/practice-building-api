d544e41dca7669ae4139c3acb4c5c3f0d40f4312
phatdang <phatdang19052004@gmail.com>
2025-08-15 20:39:56 +0700

    feat(util.request): thêm class AuthenticationRequest
    Thêm class AuthenticationRequest để tạo request đăng nhập từ user

---
+++ src/main/java/fit/iuh/se/buildingapi/dto/request/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package fit.iuh.se.buildingapi.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class AuthenticationRequest {
+    String username;
+    String password;
+}
