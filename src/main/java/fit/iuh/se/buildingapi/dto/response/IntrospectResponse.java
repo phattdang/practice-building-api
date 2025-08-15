325b10dd478cf8b48a87170766c5271a8e8ea4bd
phatdang <phatdang19052004@gmail.com>
2025-08-15 22:06:49 +0700

    feat(dto): thêm dto để xác thực token và phản hồi
    Thêm dto để xác thực token và phản hồi

---
+++ src/main/java/fit/iuh/se/buildingapi/dto/response/IntrospectResponse.java
@@ -0,0 +1,15 @@
+package fit.iuh.se.buildingapi.dto.response;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IntrospectResponse {
+    Boolean valid;
+}
